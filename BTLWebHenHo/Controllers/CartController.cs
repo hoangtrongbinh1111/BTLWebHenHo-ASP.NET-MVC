b47e8d6f09bccb2e5c9f28eb40a575c71df41b8f
Hoàng Trọng Bình <62093518+hoangtrongbinh1111@users.noreply.github.com>
2020-10-19 13:51:33 +0700

    commit_3 19/10
    chỉnh lại cart controller

---
+++ BTLWebHenHo/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTLWebHenHo.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult payment()
+        {
+            return View();
+        }
+          public ActionResult buy_coin()
+          {
+               return View();
+          }
+          
+          public ActionResult shop()
+          {
+               return View();
+          }
+          public ActionResult CheckOut()
+          {
+               return View();
+          }
+     }
+}
