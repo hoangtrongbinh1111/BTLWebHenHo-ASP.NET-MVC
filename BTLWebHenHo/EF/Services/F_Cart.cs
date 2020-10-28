a2078ac621a368597159367922bcb4b56448fad8
BlackholeMTA <69639992+BlackholeMTA@users.noreply.github.com>
2020-10-28 15:49:19 +0700

    sd

---
+++ BTLWebHenHo/EF/Services/F_Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BTLWebHenHo.EF.Infrastructure;
+using BTLWebHenHo.Models;
+namespace BTLWebHenHo.EF.Services
+{
+    public class F_Cart : RepositoryBase<tbl_products>
+    {
+         
+    }
+}
